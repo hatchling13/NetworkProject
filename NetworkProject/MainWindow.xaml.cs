d7a54cb468ebefca7038c11b017aad7b272865b7
Jeong Wook Park <hatchling3713@gmail.com>
2021-05-24 22:46:05 +0900

    Applied AdonisUI to project

--- NetworkProject/MainWindow.xaml.cs
+++ NetworkProject/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace NetworkProject
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : AdonisUI.Controls.AdonisWindow
     {
         public MainWindow()
         {
